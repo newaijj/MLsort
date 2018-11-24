935e4752e3b2367b2093db8f1ddf81cda18fc60a
newaij <poontzeyang@gmail.com>
2018-11-24 11:51:28 +0800

    Sigmoid sort attempt 2
    still has 0 accuracy, probably needs more layers

--- MLsort_sigmoid/MLsort.m
+++ MLsort_sigmoid/MLsort.m
@@ -1,24 +1,27 @@
 %% Initialization
 clear ; close all; clc;
 
-load small_X; load small_X_sorted_indices;
+load('data/X.mat');
+load('data/X_sorted_indices.mat');
 
-X = small_X;
-y = small_X_sorted_indices;
+X = X/100;
+y = X_sorted_indices/10;
 
 
 m = size(X, 1);
 
 input_layer_size  = 10;  % 10 numbers
-hidden_layer_size = 10;   % 10 hidden units
+hidden_layer_size_1 = 50;   % hidden units
+%hidden_layer_size_2 = 50
 num_labels = 10;          % 10 sorted numbers
 
 % Weight regularization parameter
-lambda = 1;
+lambda = 0;
 
 % Unroll parameters 
-Theta1 = randInitializeWeights(input_layer_size + 1, hidden_layer_size);
-Theta2 = randInitializeWeights(hidden_layer_size + 1, num_labels);
+Theta1 = randInitializeWeights(input_layer_size + 1, hidden_layer_size_1);
+Theta2 = randInitializeWeights(hidden_layer_size_1 + 1, hidden_layer_size_2);
+%Theta3 = randInitializeWeights(hidden_layer_size_2 + 1, num_labels);
 initial_nn_params = [Theta1(:) ; Theta2(:)];               
              
 
@@ -37,30 +40,32 @@ initial_nn_params = [Theta1(:) ; Theta2(:)];
 % Create short hand for the cost function to be minimized
 costFunction = @(p) nnCostFunction(p, ...
                                    input_layer_size, ...
-                                   hidden_layer_size, ...
+                                   hidden_layer_size_1, ...
                                    num_labels, X, y, lambda);
                                    
 %  After you have completed the assignment, change the MaxIter to a larger
 %  value to see how more training helps.
-options = optimset('MaxIter', 50);
+options = optimset('MaxIter', 100);
 
 % Now, costFunction is a function that takes in only one argument (the
 % neural network parameters)
 [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
 % Obtain Theta1 and Theta2 back from nn_params
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+Theta1 = reshape(nn_params(1:hidden_layer_size_1 * (input_layer_size + 1)), ...
+                 hidden_layer_size_1, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size_1 * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size_1 + 1));
 
 %Check that the new theta is classifying correctly by testing with forward prop (predict)
-%testset = [9,9,9,8,8,2,2,1,1,1]
+%testset = [9,8,7,6,5,1,2,3,4,5]*10/100
+%testset2 = [1,2,3,4,5,6,7,8,9,9]/10
 %predict(Theta1,Theta2,testset,num_labels)
+%predict(Theta1,Theta2,testset2,num_labels)
 
 
-test = assessSigmoid(X,Theta1,Theta2,X,m,num_labels)
+result = assessSigmoid(X,Theta1,Theta2,X,m,num_labels)
 
 %gradient checking
 
--- MLsort_sigmoid/assessSigmoid.m
+++ MLsort_sigmoid/assessSigmoid.m
@@ -3,11 +3,24 @@ function accuracy = assessSigmoid(X_unsorted,Theta1,Theta2,testset,m,num_labels)
 predictedOrders = predict(Theta1,Theta2,testset,num_labels);
 
 successes = 0;
+
+%generating predicted indices from predicted orders
+predictedIndices = zeros(size(predictedOrders));
 for i = 1:m
-  [dummy, index_prediction] = sort(predictedOrders(i,:));
+  holder = sort(predictedOrders(i,:),2);
+  for j = 1:num_labels
+    predictedIndices(i,j) = min(find(holder == predictedOrders(i,j)));
+  end
+end
+  
+
+for i = 1:m
   [dummy, index_X] = sort(X_unsorted(i,:));
-  if (index_prediction == index_X)
+  if (predictedIndices(i,:) == index_X)
     successes = successes + 1;
-  endif
-endfor
+  end
+end
+
 accuracy = successes/m;
+
+end
